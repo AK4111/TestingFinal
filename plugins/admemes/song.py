a5e716d419d4f8974266d0e878244b0e2a2b9859
AK4111 <98637357+AK4111@users.noreply.github.com>
2022-06-29 16:38:37 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -49,7 +49,7 @@ def song(client, message):
         open(thumb_name, 'wb').write(thumb.content)
 
 
-        performer = f"[ᗷᗩᗞᔑᕼᗩ]" 
+        performer = f"[AK PRO]" 
         duration = results[0]["duration"]
         url_suffix = results[0]["url_suffix"]
         views = results[0]["views"]
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ʙᴀᴅsʜᴀ sᴛᴜᴅɪᴏs Yt](https://youtube.com/channel/UCFjqpS7MmN42sybrG0Vr0NQ)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [ʙᴀᴅsʜᴀ sᴛᴜᴅɪᴏs](https://t.me/Badsha_Studios)**'
+        rep = '**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [Movies Express](https://t.me/Moviez_Express_Official)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
